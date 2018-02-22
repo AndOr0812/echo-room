391b9bd1dc0644e6973e7fc2502de3a59a109cdb
Alejandro Villarreal <avillarreal01@gmail.com>
2018-02-22 11:46:51 -0500

    add socket for audio stream

--- src/components/Audio/Audio.js
+++ src/components/Audio/Audio.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import socket from '../../socket'
 
 const audioTest = () => {
   var handleSuccess = function(stream) {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+    const audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)()
     const source = audioContext.createMediaStreamSource(stream)
     const processor = audioContext.createScriptProcessor(1024, 1, 1)
 
@@ -11,15 +13,17 @@ const audioTest = () => {
 
     processor.onaudioprocess = function(e) {
       console.log(e.inputBuffer)
+      socket.emit('transmitAudio', 'audio sample')
     }
   }
-
-  navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess)
+  navigator.mediaDevices
+    .getUserMedia({ audio: true, video: false })
+    .then(handleSuccess)
 }
 
 const audio = ({ props }) => {
   audioTest()
-  return <div>I am here</div>
+  return <div>Testing Audio...</div>
 }
 
 export default audio
